bcdaa2401d96fdbc8aa54438078e48f2482add95
badger <yael.calleja321@gmail.com>
2026-02-01 19:25:03 -0600

    "Archivo default para cada branch y comentado"

--- scriptdecreacion.sql
+++ scriptdecreacion.sql
@@ -1,50 +1,8 @@
-#CREATE DATABASE Universidad;
+# Eliminar base de datos en caso de existir
+DROP DATABASE IF EXISTS Universidad;
+
+# Creacion de la base de datos
+CREATE DATABASE Universidad;
+
+# Seleccionamos la base de datos
 USE Universidad;
-
-CREATE TABLE Estudiantes(
-codStud	INT auto_increment primary key,
-nombre	VARCHAR(50), 
-carrera	VARCHAR(20),
-promedio	DECIMAL(4,2),
-genero	VARCHAR(10), 
-nacimiento	DATE
-);  
-
-create table docentes(
-codProf
-
-)
-
-create table departamentos(
-idDep
-    
-)
-
-create table administrativos(
-idAdm
-    
-)
-
-
-create table servicios_gen(
-
-
-)
-
-create table finanzas(
-
-    
-)
-
-create table control_esc(
-
-    
-)
-
-
-
-INSERT	INTO Estudiantes(nombre,carrera,promedio,genero,nacimiento)
-VALUES
-("PIPOPE","IA",60.00,"INDEFINIDO",12-10-2000);
-SELECT * FROM Estudiantes;
-#DROP TABLE Estudiantes;
